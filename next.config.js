0f39cc5b2bfff3745b54bca69a685600684b94d6
Sohrab-tech1 <127429764+Sohrab-tech1@users.noreply.github.com>
2025-01-01 22:37:33 +0100

    import the esm package

--- next.config.js
+++ next.config.js
@@ -6,6 +6,18 @@ const nextConfig = {
   images: {
     domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
   },
+  experimental: {
+    esmExternals: 'loose'
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        'supports-color': false,
+      }
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
